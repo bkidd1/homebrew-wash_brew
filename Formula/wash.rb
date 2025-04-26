51f65e92380007a36f711eafc5d64134929c56a5
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-04-26 16:46:00 -0500

    Update wash formula to version 1.0.3

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,8 +1,8 @@
 class Wash < Formula
   desc "A development assistant that helps track errors, decisions, and project state"
   homepage "https://github.com/bkidd1/wash-cli"
-  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "81815efb5bd04583d703bf679d0b9d5ed838a655236bb33a4feec145b556efea"
+  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "5ac938306d802ecf485b46f992043ac35f8173b9ee60dceb4641fe47b652d103"
   license "MIT"
 
   depends_on "go" => :build
@@ -12,6 +12,6 @@ class Wash < Formula
   end
 
   test do
-    assert_match "v1.0.2", shell_output("#{bin}/wash version")
+    assert_match "v1.0.3", shell_output("#{bin}/wash version")
   end
 end
