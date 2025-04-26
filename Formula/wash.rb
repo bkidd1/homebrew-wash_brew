575591a81bf8b89325778ca9901978595cb89b54
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-04-26 08:47:00 -0500

    Update wash formula to version 0.1.1

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,17 +1,17 @@
 class Wash < Formula
-    desc "CLI tool for managing your washing machine"
-    homepage "https://github.com/bkidd1/wash-cli"
-    url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "e56dc64fcbc9e415956454b703a02462321c1cf7efa205796a7b1a6fe31fb2d1"
-    license "MIT"
-  
-    depends_on "go" => :build
-  
-    def install
-      system "go", "build", "-trimpath", "-o", bin/"wash", "./cmd/wash"
-    end
-  
-    test do
-      assert_match "dev", shell_output("#{bin}/wash version")
-    end
+  desc "A development assistant that helps track errors, decisions, and project state"
+  homepage "https://github.com/bkidd1/wash-cli"
+  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "b8171b933b7ef80d38006bb62b12669456b287780a7c0bd3e044ee4bb72b6301"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"wash", "./cmd/wash"
   end
+
+  test do
+    assert_match "v0.1.1", shell_output("#{bin}/wash version")
+  end
+end
