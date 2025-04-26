80e5458e41741ae56115bcc08b63c8104634bfd3
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-04-26 08:53:24 -0500

    Update wash formula SHA256 hash

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -2,7 +2,7 @@ class Wash < Formula
   desc "A development assistant that helps track errors, decisions, and project state"
   homepage "https://github.com/bkidd1/wash-cli"
   url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "add467fbc5a5f3f9e21154747d33ac68e29d2b2c9f736b0e349eed20adb7d6e2"
+  sha256 "81815efb5bd04583d703bf679d0b9d5ed838a655236bb33a4feec145b556efea"
   license "MIT"
 
   depends_on "go" => :build
