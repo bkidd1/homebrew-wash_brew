b6f7359f2682b55d0acfd31687d55e208a78a5da
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-05-01 16:07:31 -0700

    Update wash formula to version 1.0.8 with new SHA256 hash

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,29 +1,29 @@
 class Wash < Formula
   desc "A development assistant that helps track errors, decisions, and project state"
   homepage "https://github.com/bkidd1/wash-cli"
-  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "ac2547d8443dfcdfc2bc64857b72c581f15b5f25dc27e6351e3c975f259e4d09"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Darwin_x86_64.tar.gz"
-      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.8/wash-cli_Darwin_x86_64.tar.gz"
+      sha256 "ac2547d8443dfcdfc2bc64857b72c581f15b5f25dc27e6351e3c975f259e4d09"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Darwin_arm64.tar.gz"
-      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.8/wash-cli_Darwin_arm64.tar.gz"
+      sha256 "ac2547d8443dfcdfc2bc64857b72c581f15b5f25dc27e6351e3c975f259e4d09"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Linux_x86_64.tar.gz"
-      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.8/wash-cli_Linux_x86_64.tar.gz"
+      sha256 "ac2547d8443dfcdfc2bc64857b72c581f15b5f25dc27e6351e3c975f259e4d09"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Linux_arm64.tar.gz"
-      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.8/wash-cli_Linux_arm64.tar.gz"
+      sha256 "ac2547d8443dfcdfc2bc64857b72c581f15b5f25dc27e6351e3c975f259e4d09"
     end
   end
 
@@ -32,6 +32,6 @@ class Wash < Formula
   end
 
   test do
-    assert_match "v1.0.7", shell_output("#{bin}/wash version")
+    assert_match "v1.0.8", shell_output("#{bin}/wash version")
   end
 end
