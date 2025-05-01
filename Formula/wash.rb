644bce4b879e1685498311e679f9c529183a8fd4
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-05-01 15:53:31 -0700

    Update wash formula to version 1.0.6

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,17 +1,37 @@
 class Wash < Formula
   desc "A development assistant that helps track errors, decisions, and project state"
   homepage "https://github.com/bkidd1/wash-cli"
-  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "5ac938306d802ecf485b46f992043ac35f8173b9ee60dceb4641fe47b652d103"
+  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Darwin_x86_64.tar.gz"
+      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Darwin_arm64.tar.gz"
+      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Linux_x86_64.tar.gz"
+      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Linux_arm64.tar.gz"
+      sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
+    end
+  end
 
   def install
-    system "go", "build", "-o", bin/"wash", "./cmd/wash"
+    bin.install "wash"
   end
 
   test do
-    assert_match "v1.0.3", shell_output("#{bin}/wash version")
+    assert_match "v1.0.6", shell_output("#{bin}/wash version")
   end
 end
