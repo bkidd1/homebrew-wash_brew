c3f3991b5df140d173d7d17e2b9dd77389881c19
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-05-01 16:04:31 -0700

    Update wash formula to version 1.0.7

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -1,28 +1,28 @@
 class Wash < Formula
   desc "A development assistant that helps track errors, decisions, and project state"
   homepage "https://github.com/bkidd1/wash-cli"
-  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.6.tar.gz"
+  url "https://github.com/bkidd1/wash-cli/archive/refs/tags/v1.0.7.tar.gz"
   sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Darwin_x86_64.tar.gz"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Darwin_x86_64.tar.gz"
       sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Darwin_arm64.tar.gz"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Darwin_arm64.tar.gz"
       sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Linux_x86_64.tar.gz"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Linux_x86_64.tar.gz"
       sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.6/wash-cli_Linux_arm64.tar.gz"
+      url "https://github.com/bkidd1/wash-cli/releases/download/v1.0.7/wash-cli_Linux_arm64.tar.gz"
       sha256 "8192a631690cca417c0bf5609e2bd8fe4f3e04d2e2e7370dfa96858644b742b7"
     end
   end
@@ -32,6 +32,6 @@ class Wash < Formula
   end
 
   test do
-    assert_match "v1.0.6", shell_output("#{bin}/wash version")
+    assert_match "v1.0.7", shell_output("#{bin}/wash version")
   end
 end
