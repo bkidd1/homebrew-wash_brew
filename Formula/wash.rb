bfff4c2330428efc273c5786cd18bc3971060d69
Brinlee Kidd <90875591+bkidd1@users.noreply.github.com>
2025-05-09 08:03:53 -0700

    Update SHA256 hashes for all platform variants

--- Formula/wash.rb
+++ Formula/wash.rb
@@ -6,20 +6,20 @@ class Wash < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/bkidd1/wash-cli/releases/download/v1.1.2/wash-cli_Darwin_arm64.tar.gz"
-      sha256 "118662ea700814f8f4699105bbaed92bd65763df7c9268f04a6d41cac8d9b6c9"
+      sha256 "db46f6769af61ebe220032ad91339f23401918928f431170395977b8a3c8e895"
     else
       url "https://github.com/bkidd1/wash-cli/releases/download/v1.1.2/wash-cli_Darwin_x86_64.tar.gz"
-      sha256 "c01f59dab621e26c2e276ed0c2948d9ea4c79f1fe0a1e649e56d1a1fc68eef87"
+      sha256 "db46f6769af61ebe220032ad91339f23401918928f431170395977b8a3c8e895"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/bkidd1/wash-cli/releases/download/v1.1.2/wash-cli_Linux_arm64.tar.gz"
-      sha256 "42b4c547b550a689e209d549c7e5958b5b6cccdef685f4800c7a185d88ef67ed"
+      sha256 "db46f6769af61ebe220032ad91339f23401918928f431170395977b8a3c8e895"
     else
       url "https://github.com/bkidd1/wash-cli/releases/download/v1.1.2/wash-cli_Linux_x86_64.tar.gz"
-      sha256 "c60f3f4599480b90a0036f52b03389c736b2a274504687d9d18bc441d6d97bd4"
+      sha256 "db46f6769af61ebe220032ad91339f23401918928f431170395977b8a3c8e895"
     end
   end
 
